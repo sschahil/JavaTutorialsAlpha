d6be0f3eb9083aaf34c327cef39aec0b2e3c092e
Sumant Chahil <schahil31@gmail.com>
2017-05-28 11:47:21 -0700

    Lesson 7

---
+++ Java_Tutorial_7/src/JavaLessonSeven.java
@@ -0,0 +1,13 @@
+
+public class JavaLessonSeven {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Monster Frank = new Monster();
+		Frank.name = "Frank";
+		
+		System.out.println(Frank.name + " has an attack of " + Frank.getAttack());
+	}
+
+}
