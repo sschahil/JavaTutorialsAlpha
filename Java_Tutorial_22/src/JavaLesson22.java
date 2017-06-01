1d417bbf6438a06d21c659bd3b102d21f1d6dd94
Sumant Chahil <schahil31@gmail.com>
2017-05-31 17:49:04 -0700

    Lesson 22 - Java Swing GUI Part 3

---
+++ Java_Tutorial_22/src/JavaLesson22.java
@@ -0,0 +1,108 @@
+import javax.swing.*;
+import java.awt.event.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import java.text.NumberFormat;
+import javax.swing.border.*;
+
+public class JavaLesson22 extends JFrame{
+
+	JButton button1;
+	JLabel label1, label2, label3;
+	JTextField textField1, textField2;
+	JCheckBox dollarSign, commaSeparator;
+	JRadioButton addNums, subtractNums, multNums, divideNums;
+	JSlider howManyTimes;
+	
+	double number1, number2, totalCalc;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new JavaLesson22();
+	}
+
+	public JavaLesson22() {
+		
+		this.setSize(400,400);
+		this.setLocationRelativeTo(null);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setTitle("My Third Frame");
+		
+		JPanel thePanel = new JPanel();
+		
+		button1 = new JButton("Calculate");
+		
+		ListenForButton lForButton = new ListenForButton();
+		
+		button1.addActionListener(lForButton);
+		
+		thePanel.add(button1);
+		
+		label1 = new JLabel("Number 1");
+		thePanel.add(label1);
+		textField1 = new JTextField("", 5);
+		thePanel.add(textField1);
+		
+		label2 = new JLabel("Number 2");
+		thePanel.add(label2);
+		textField2 = new JTextField("", 5);
+		thePanel.add(textField2);
+		
+		dollarSign = new JCheckBox("Dollars");
+		commaSeparator = new JCheckBox("Commas");
+		
+		thePanel.add(dollarSign);
+		thePanel.add(commaSeparator);
+		
+		addNums = new JRadioButton("Add");
+		subtractNums = new JRadioButton("Subtract");
+		multNums = new JRadioButton("Multiply");
+		divideNums = new JRadioButton("Divide");
+		
+		ButtonGroup operation = new ButtonGroup();
+		
+		operation.add(addNums);
+		operation.add(subtractNums);
+		operation.add(multNums);
+		operation.add(divideNums);
+		
+		JPanel operPanel = new JPanel();
+		
+		Border operBorder = BorderFactory.createTitledBorder("Operation");
+		
+		operPanel.setBorder(operBorder);
+
+		operPanel.add(addNums);
+		operPanel.add(subtractNums);
+		operPanel.add(multNums);
+		operPanel.add(divideNums);
+		
+		addNums.setSelected(true);
+		
+		thePanel.add(operPanel);
+		
+		label3 = new JLabel("Perform How Many Times");
+		
+		thePanel.add(label3);
+		
+		howManyTimes = new JSlider(0, 99, 1);
+		
+		howManyTimes.setMinorTickSpacing(1);
+		howManyTimes.setMajorTickSpacing(10);
+		howManyTimes.setPaintTicks(true);
+		howManyTimes.setPaintLabels(true);
+		
+		ListenForSlider lForSlider = new ListenForSlider();
+		
+		howManyTimes.addChangeListener(lForSlider);
+		
+		thePanel.add(howManyTimes);
+		
+		this.add(thePanel);
+		this.setVisible(true);
+		
+		textField1.requestFocus();
+		
+	}
+	
+}
